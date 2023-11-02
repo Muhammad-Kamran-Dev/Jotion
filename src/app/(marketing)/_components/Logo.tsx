08f8678d8211806d8decaac39488dbb243e5517a
Muhammad Kamran <MuhammadKamran.dev@gmail.com>
2023-11-02 22:32:17 +0500

    Landing Page done with darkmode and proper directory structure

---
+++ src/app/(marketing)/_components/Logo.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "600"],
+});
+const Logo = () => {
+  return (
+    <div className="hidden md:flex items-center gap-x-2">
+      <Image
+        src="/logo.svg"
+        width={40}
+        height={40}
+        alt="Logo"
+        className="dark:hidden"
+      />
+      <Image
+        src="/logo-dark.svg"
+        width={40}
+        height={40}
+        alt="Logo"
+        className="hidden dark:block"
+      />
+      <p className={cn("font-semibold", poppins.className)}> Jotion</p>
+    </div>
+  );
+};
+
+export default Logo;
